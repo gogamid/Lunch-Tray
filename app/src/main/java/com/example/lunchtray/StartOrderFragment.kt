6ddb8073d39198292a8ac818824ad67ed67b8a31
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-14 20:04:56 +0200

    feat: implement entree fragment with navigation graph when start order is clicked

--- app/src/main/java/com/example/lunchtray/StartOrderFragment.kt
+++ app/src/main/java/com/example/lunchtray/StartOrderFragment.kt
@@ -5,18 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.lunchtray.databinding.FragmentStartOrderBinding
 
 class StartOrderFragment : Fragment() {
+  private lateinit var binding: FragmentStartOrderBinding
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    return FragmentStartOrderBinding.inflate(inflater, container, false).root
+    binding = FragmentStartOrderBinding.inflate(inflater, container, false)
+    return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    binding.startOrderButton.setOnClickListener {
+      this.findNavController().navigate(R.id.action_startOrderFragment_to_entreeFragment)
+    }
   }
 }

5021a322d13bd75c7da43ea66d6a8a1dc5af74a2
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-18 13:47:14 +0200

    feat: implement price calculation

--- app/src/main/java/com/example/lunchtray/SummaryFragment.kt
+++ app/src/main/java/com/example/lunchtray/SummaryFragment.kt
@@ -5,34 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.example.lunchtray.databinding.FragmentSideDishBinding
 import com.example.lunchtray.databinding.FragmentSummaryBinding
+import com.example.lunchtray.model.OrderViewModel
 
 
 class SummaryFragment : Fragment() {
-
+  private val sharedViewModel: OrderViewModel by activityViewModels()
   private lateinit var binding: FragmentSummaryBinding
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
-    // Inflate the layout for this fragment
+  ): View {
     binding = DataBindingUtil.inflate(inflater, R.layout.fragment_summary, container, false)
     return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
-    binding.summaryFragment = this
+    binding.apply {
+      summaryFragment = this@SummaryFragment
+      viewModel = sharedViewModel
+      lifecycleOwner = viewLifecycleOwner
+    }
   }
 
   fun submitOrder() {
+    Toast.makeText(requireContext(), "Order Submitted", Toast.LENGTH_SHORT).show()
+    sharedViewModel.resetOrder()
     this.findNavController().navigate(R.id.action_summaryFragment_to_startOrderFragment)
   }
 
   fun cancelOrder() {
+    sharedViewModel.resetOrder()
     this.findNavController().navigate(R.id.action_summaryFragment_to_startOrderFragment)
   }
 }
--- app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
+++ app/src/main/java/com/example/lunchtray/model/OrderViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.lunchtray.model
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
@@ -8,23 +7,35 @@ import androidx.lifecycle.ViewModel
 import com.example.lunchtray.data.Datasource
 import java.text.NumberFormat
 
+private const val TAX_RATE = 0.084
+
 class OrderViewModel : ViewModel() {
   val data = Datasource()
   val entrees = data.getEntrees()
   val sideDishes = data.getSideDishes()
   val accompaniments = data.getAccompaniments()
 
-  private val _entree = MutableLiveData<Entree>()
-  val entree: LiveData<Entree> = _entree
+  private val _entree = MutableLiveData<Entree?>()
+  val entree: MutableLiveData<Entree?> = _entree
 
-  private val _sideDish = MutableLiveData<SideDish>()
-  val sideDish: LiveData<SideDish> = _sideDish
+  private val _sideDish = MutableLiveData<SideDish?>()
+  val sideDish: MutableLiveData<SideDish?> = _sideDish
 
-  private val _accompaniment = MutableLiveData<Accompaniment>()
-  val accompaniment: LiveData<Accompaniment> = _accompaniment
+  private val _accompaniment = MutableLiveData<Accompaniment?>()
+  val accompaniment: MutableLiveData<Accompaniment?> = _accompaniment
 
   private val _price = MutableLiveData<Double>()
   val price: LiveData<String> = Transformations.map(_price) {
+    NumberFormat.getCurrencyInstance().format(it)
+  }
+
+  private val _tax = MutableLiveData<Double>()
+  val tax: LiveData<String> = Transformations.map(_tax) {
+    NumberFormat.getCurrencyInstance().format(it)
+  }
+
+  private val _total = MutableLiveData<Double>()
+  val total: LiveData<String> = Transformations.map(_total) {
     NumberFormat.getCurrencyInstance().format(it)
   }
 
@@ -33,23 +44,33 @@ class OrderViewModel : ViewModel() {
   }
 
   fun resetOrder() {
-    _entree.value = entrees[0]
-    _sideDish.value = sideDishes[0]
-    _accompaniment.value = accompaniments[0]
-//    _price.value = entrees[0].price
+    _entree.value = null
+    _sideDish.value = null
+    _accompaniment.value = null
+    updatePrice()
   }
 
   fun setEntree(entree: Entree) {
     _entree.value = entree
-//    _price.value = entree.price
+    updatePrice()
+  }
+
+  private fun updatePrice() {
+    val entreePrice: Double = _entree.value?.price ?: 0.0
+    val sideDishPrice: Double = _sideDish.value?.price ?: 0.0
+    val accompanimentPrice: Double = _accompaniment.value?.price ?: 0.0
+    _price.value = entreePrice + sideDishPrice + accompanimentPrice
+    _tax.value = if (_price.value == null) 0.0 else _price.value!! * TAX_RATE
+    _total.value = if (_price.value == null) 0.0 else _price.value!! + _tax.value!!
   }
 
   fun setSideDish(sideDish: SideDish) {
     _sideDish.value = sideDish
-//    _price.value = _price.value?.plus(sideDish.price)
+    updatePrice()
   }
 
   fun setAccompaniment(accompaniment: Accompaniment) {
     _accompaniment.value = accompaniment
+    updatePrice()
   }
 }

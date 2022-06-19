cbc9f0c8850534428c4685a2a6ac1f5121c857ba
Imron <imron.gamidli@lt.hs-fulda.de>
2022-06-19 22:12:14 +0200

    test(ui): each meal different price

--- app/src/androidTest/java/com/example/lunchtray/OrderFlowTests.kt
+++ app/src/androidTest/java/com/example/lunchtray/OrderFlowTests.kt
@@ -21,7 +21,7 @@ import org.junit.Rule
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class OrderFlowTests {
 
   @get:Rule()
   val activity = ActivityScenarioRule(MainActivity::class.java)
@@ -44,5 +44,25 @@ class ExampleInstrumentedTest {
       .perform(click())
     onView(withId(R.id.total))
       .check(matches(withSubstring("10,84")))
+  }
+
+  @Test
+  fun each_meal_different_price() {
+    onView(withId(R.id.start_order_button))
+      .perform(click())
+    onView(withId(R.id.subtotal))
+      .check(matches(withSubstring("7,00")))
+    onView(withId(R.id.entree_1))
+      .perform(click())
+    onView(withId(R.id.subtotal))
+      .check(matches(withSubstring("4,00")))
+    onView(withId(R.id.entree_2))
+      .perform(click())
+    onView(withId(R.id.subtotal))
+      .check(matches(withSubstring("3,00")))
+    onView(withId(R.id.entree_3))
+      .perform(click())
+    onView(withId(R.id.subtotal))
+      .check(matches(withSubstring("2,00")))
   }
 }
